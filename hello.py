a48afff29e718ac010e78f04ec5d444e4f4ee99e
wangxun <troy.wang@beyondsoft.com>
2015-03-19 16:26:52 +0800

    2323

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, session, redirect, url_for
-from flask_script import Manager
-from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from flask_wtf import Form
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
@@ -33,6 +33,9 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
