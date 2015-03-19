a1991d1472a8e9adad94a7875c946da53c4dd68d
wangxun <troy.wang@beyondsoft.com>
2015-03-19 18:21:24 +0800

    test

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,9 @@ from flask_moment import Moment
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -18,5 +21,14 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db =SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 from app import views
